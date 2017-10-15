059f7879b7936ed01f89243acc47fc77cf2348fd
Stephen McAuley <steviebiddles@gmail.com>
2017-10-15 22:26:16 +0100

    Lesson Three

---
+++ LessonThree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LessonThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Stephen";
+            string greeting = $"Hello from lesson three {name}!";
+            string level = "loud";
+
+            if (level == "loud")
+            {
+                greeting = greeting.ToUpper();
+            }
+            else if (level == "quite")
+            {
+                greeting = greeting.ToLower();
+            }
+
+            Console.WriteLine(greeting);
+            Console.ReadLine();
+        }
+    }
+}
