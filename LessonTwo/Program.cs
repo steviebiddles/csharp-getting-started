c9e61a7eb1eb6cd9a7db602e76d9f3e83ff97eec
Stephen McAuley <steviebiddles@gmail.com>
2017-10-15 22:11:05 +0100

    Lesson Two

---
+++ LessonTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LessonTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Stephen";
+            var greeting = $"Hello {name}";
+
+            Console.WriteLine(greeting);
+            Console.ReadLine();
+        }
+    }
+}
